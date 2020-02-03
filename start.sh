783cc95d95570bacfc0deca10c5ade69942994ce
EtherCore Contributor <info@ethercore.org>
2020-02-04 04:52:32 +0900

    Update scripts

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo systemctl start parity geth geth2 netstats blockscout
+sudo systemctl start parity geth geth2 netstats blockscout && cd eth-net-intelligence-api && pm2 start app.json app2.json app3.json
